7ac8aea5941206a084db077df2b9fa559b339ba3
IvanR314 <Ivanrhodes15@gmail.com>
2025-03-04 16:22:02 -0600

    Basic implementations of methods. Attributes for CharacterManager and MiddleEarthCouncil classes.

--- The_Middle_Earth_Madness/src/middleearth/management/CharacterManager.java
+++ The_Middle_Earth_Madness/src/middleearth/management/CharacterManager.java
@@ -1,5 +1,90 @@
 package middleearth.management;
 
 public class CharacterManager {
-
+	MiddleEarthCharacter[] characters = new MiddleEarthCharacter[5]; 
+	private int size;
+	
+	public boolean addCharacter(MiddleEarthCharacter c) 
+	{
+		for(int i = 0; i < size; i++) 
+		{
+			if(characters[i] == null) {
+				characters[i] = c;
+				size++;
+				return true;
+			}
+		}
+	return false;	
+	}
+	
+	MiddleEarthCharacter getCharacter(String name) 
+	{
+		for(int i = 0; i < size; i++) 
+		{
+			if(character[i].name == name) 
+			{
+				MiddleEarthCharacter copy = new MiddleEarthCharacter(character[i]);
+				return copy;
+			}
+		}
+	}
+	
+	boolean updateCharacter (MiddleEarthCharacter character, String name, int health, int power) 
+	{
+		int check = 0;
+		for(int i = 0; i < size; i++) 
+		{
+			if (characters[i] == character)
+			{
+				if(characters[i].name != name) 
+				{
+					characters[i].name = name;
+					check++;
+				}
+				
+				if(characters[i].health != health) 
+				{
+					characters[i].health = health;
+					check++;
+				}
+				
+				if(characters[i].power != power) 
+				{
+					characters[i].power = power;
+					check++;
+				}
+			}
+			
+		}
+		if(check > 0) 
+		{
+			return true;
+		}
+			
+		else 
+		{
+			return false;
+		}
+	}
+	
+	
+	boolean deleteCharacter(MiddleEarthCharacter character) 
+	{
+		for (int i = 0; i < size; i++) 
+		{
+			if(characters[i] == character) 
+			{
+				
+			}
+		}
+	}
+	
+	public void displayAllCharacters() 
+	{
+		for(MiddleEarthCharacter character: characters)
+		{
+			if (character != null) {
+				character.displayInfo();
+			}	
+	}
 }
--- The_Middle_Earth_Madness/src/middleearth/management/MiddleEarthCouncil.java
+++ The_Middle_Earth_Madness/src/middleearth/management/MiddleEarthCouncil.java
@@ -2,4 +2,19 @@ package middleearth.management;
 
 public class MiddleEarthCouncil {
 
+	private static MiddelEarthCouncil instance;
+	
+	private MiddleEarthCouncil() {}
+	
+	public static MiddleEarthCouncil getInstance() 
+	{
+		if (instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance;
+	}
+	
+	public static CharacterManager getCharacterManager() {
+		
+	}
 }
