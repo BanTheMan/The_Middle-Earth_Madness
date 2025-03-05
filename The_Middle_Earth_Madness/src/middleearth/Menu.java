4f549a0d152a69444428b72d01ec207d0ee73e74
ngsk6y <ngsk6y@umsystem.edu>
2025-03-05 15:53:47 -0600

    Updated menu
    updated the menu class with adding new methods for easier functionality

--- The_Middle_Earth_Madness/src/middleearth/Menu.java
+++ The_Middle_Earth_Madness/src/middleearth/Menu.java
@@ -10,63 +10,78 @@ public class Menu {
 
 
 
-public Menu() {
-	this.scanner = new Scanner(System.in);
-	this.characterManager = new CharacterManager();
-}
-
-
-public void displayMenu() {
-	while (true) {
-		System.out.println("\n --== Middle Earth Character Menu ==--\n");
-		System.out.println("1. Add a new character.\n");
-		System.out.println("2. View all characters.\n");
-		System.out.println("3. Update a character.\n");
-		System.out.println("4. Delete a character.\n");
-		System.out.println("5. Execute all characters' attack actions.\n");
-		System.out.println("6. Exit.\n");
-		System.out.println("Please enter an input: \n");
+	public Menu() {
+		this.scanner = new Scanner(System.in);
+		this.characterManager = new CharacterManager();
+	}
+	
+	public String selectCharacter() {
+		System.out.println("Please input the character you wish to select.\n");
+		String characterName = scanner.next();
+		return characterName;
+				
+	}
+	
+	public String createCharacter() {
 		
-		int choice = scanner.nextInt();
-		scanner.nextLine();
 		
-		switch (choice) {
-			case 1: // Consider asking them to choose a race and amount of health & power
-				System.out.println("Input 1 selected.\n");
-				characterManager.addCharacter(); // Use given information to create and pass a character instance
-				break;
+		
+		
+	}
+	
+	public void displayMenu() {
+		while (true) {
+			System.out.println("\n --== Middle Earth Character Menu ==--\n");
+			System.out.println("1. Add a new character.\n");
+			System.out.println("2. View all characters.\n");
+			System.out.println("3. Update a character.\n");
+			System.out.println("4. Delete a character.\n");
+			System.out.println("5. Execute all characters' attack actions.\n");
+			System.out.println("6. Exit.\n");
+			System.out.println("Please enter an input: \n");
 			
-			case 2:
-				System.out.println("Input 2 selected.\n");
-				characterManager.displayAllCharacters();
-				break;
+			int choice = scanner.nextInt();
+			scanner.nextLine();
 			
-			case 3: // Consider given the user the option to choose which character to update
-				System.out.println("Input 3 selected.\n");
-				characterManager.updateCharacter(); // Ask and pass the character and stats to update
-				break;
+			switch (choice) {
+				case 1: // Consider asking them to choose a race and amount of health & power
+					System.out.println("Input 1 selected.\n");
+					characterManager.addCharacter(); // Use given information to create and pass a character instance
+					break;
 				
-			case 4:
-				System.out.println("Input 4 selected.\n");
-				characterManager.deleteCharacter(); // Ask for and pass appropriate character
-				break;
+				case 2:
+					System.out.println("Input 2 selected.\n");
+					characterManager.displayAllCharacters();
+					break;
 				
-			case 5: // Consider creating a an interface for selecting who attacks who
-				System.out.println("Input 5 selected.\n");
-				characterManager.executeAttacks(); // No function exists, may need to call individually
-				break;
+				case 3: // Consider given the user the option to choose which character to update
+					System.out.println("Input 3 selected.\n");
+					// Select a character 
+					String givenName = selectCharacter();
+					characterManager.updateCharacter(characterManager.getCharacter(givenName)); // Ask and pass the character and stats to update
+					break;
+					
+				case 4:
+					System.out.println("Input 4 selected.\n");
+					characterManager.deleteCharacter(); // Ask for and pass appropriate character
+					break;
+					
+				case 5: // Consider creating an interface for selecting who attacks who
+					System.out.println("Input 5 selected.\n");
+					characterManager.executeAttacks(); // No function exists, may need to call individually
+					break;
+					
+				case 6:
+					System.out.println("Input 6 selected.\n");
+					System.out.println("Exiting system.\n");
+					scanner.close();
+					return;
 				
-			case 6:
-				System.out.println("Input 6 selected.\n");
-				System.out.println("Exiting system.\n");
-				scanner.close();
-				return;
-			
-			default:
-				System.out.println("Invalid input. Please try again.\n");
+				default:
+					System.out.println("Invalid input. Please try again.\n");
+				}
 			}
 		}
-	}
 }
 
 	
