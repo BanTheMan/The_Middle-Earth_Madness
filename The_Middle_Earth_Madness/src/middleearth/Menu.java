99f9972df0bf5d25933397eae228a3110e061fda
ngsk6y <ngsk6y@umsystem.edu>
2025-03-05 20:44:47 -0600

    Adding character creation draft
    what my title said

--- The_Middle_Earth_Madness/src/middleearth/Menu.java
+++ The_Middle_Earth_Madness/src/middleearth/Menu.java
@@ -3,8 +3,12 @@ package middleearth;
 import java.util.Scanner;
 
 import middleearth.management.CharacterManager;
-
 import middleearth.characters.MiddleEarthCharacter;
+import middleearth.characters.Dwarf;
+import middleearth.characters.Elf;
+import middleearth.characters.Human;
+import middleearth.characters.Orc;
+import middleearth.characters.Wizard;
 
 public class Menu {
 	private Scanner scanner;
@@ -25,22 +29,85 @@ public class Menu {
 	}
 	
 	public MiddleEarthCharacter createCharacterMenu() {
-		System.out.println("Creating a new character.\n");
-		System.out.println("Please select a number to choose your character's race:\n")
-		System.out.println("1. Dwarf\n");
-		System.out.println("2. Elf\n");
-		System.out.println("3. Human\n");
-		System.out.println("4. Orc\n");
-		System.out.println("5. Wizard\n");
-		System.out.println("Please enter an input:\n");
+		System.out.println("Creating a new character.");
+		System.out.println("Please select a number to choose your character's race:");
+		System.out.println("1. Dwarf");
+		System.out.println("2. Elf");
+		System.out.println("3. Human");
+		System.out.println("4. Orc");
+		System.out.println("5. Wizard");
+		System.out.println("Please enter an input:");
 		
 		int choice = scanner.nextInt();
 		scanner.nextLine();
 		
 		switch (choice) {
-		scanner.nextLine();
+			case 1:
+				System.out.println("Please choose your Dwarf's name:");
+				String nameD = scanner.next();
+				System.out.println("Please choose your Dwarf's health:");
+				double healthD = scanner.nextDouble();
+				scanner.nextLine();
+				System.out.println("Please choose your Dwarf's power level:");
+				double powerD = scanner.nextDouble();
+				scanner.nextLine();
+				
+				MiddleEarthCharacter newDwarf = new Dwarf(nameD, healthD, powerD);
+				return newDwarf;
+
+			case 2:
+				System.out.println("Please choose your Elf's name:");
+				String nameE = scanner.next();
+				System.out.println("Please choose your Elf's health:");
+				double healthE = scanner.nextDouble();
+				scanner.nextLine();
+				System.out.println("Please choose your Elf's power level:");
+				double powerE = scanner.nextDouble();
+				scanner.nextLine();
+				
+				MiddleEarthCharacter newElf = new Elf(nameE, healthE, powerE);
+				return newElf;
+				
+			case 3:
+				System.out.println("Please choose your Human's name:");
+				String nameH = scanner.next();
+				System.out.println("Please choose your Humam's health:");
+				double healthH = scanner.nextDouble();
+				scanner.nextLine();
+				System.out.println("Please choose your Human's power level:");
+				double powerH = scanner.nextDouble();
+				scanner.nextLine();
+				
+				MiddleEarthCharacter newHuman = new Human(nameH, healthH, powerH);
+				return newHuman;
+				
+			case 4:
+				System.out.println("Please choose your Orc's name:");
+				String nameO = scanner.next();
+				System.out.println("Please choose your Orc's health:");
+				double healthO = scanner.nextDouble();
+				scanner.nextLine();
+				System.out.println("Please choose your Orc's power level:");
+				double powerO = scanner.nextDouble();
+				scanner.nextLine();
+				
+				MiddleEarthCharacter newOrc = new Orc(nameO, healthO, powerO);
+				return newOrc;
 		
+			case 5:
+				System.out.println("Please choose your Wizard's name:");
+				String nameW = scanner.next();
+				System.out.println("Please choose your Wizard's health:");
+				double healthW = scanner.nextDouble();
+				scanner.nextLine();
+				System.out.println("Please choose your Wizard's power level:");
+				double powerW = scanner.nextDouble();
+				scanner.nextLine();
+				
+				MiddleEarthCharacter newWizard = new Wizard(nameW, healthW, powerW);
+				return newWizard;
 		
+			
 		}
 	
 	}
@@ -70,7 +137,7 @@ public class Menu {
 					characterManager.displayAllCharacters();
 					break;
 				
-				case 3: // Consider given the user the option to choose which character to update
+				case 3: //DO THIS
 					System.out.println("Input 3 selected.\n");
 					
 					
